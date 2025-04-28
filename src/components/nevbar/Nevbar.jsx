a834b7441fe3ed310354df71daa8795a0d6a222f
fateme <117821877+efatemee@users.noreply.github.com>
2025-04-28 17:43:27 +0330

    first commit

---
+++ src/components/nevbar/Nevbar.jsx
@@ -0,0 +1,19 @@
+import styled from "./Nevbar.module.css";
+
+function Nevbar(props) {
+  return (
+    <div >
+      <div className="container">
+        <div className={styled.header}>
+          <h3>{props.title}</h3>
+          <ul>
+            <li>لیست مقالات</li>
+            <li>مقاله جدید </li>
+            <li>دوباره</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Nevbar;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import Nevbar from "../../components/nevbar/Nevbar";
+
+function Home() {
+  return (
+    <div>
+      <Nevbar title="سالار" />
+
+      <div className="container">
+        <h1 style={{ direction: "rtl" }} t>
+          home
+        </h1>
+      </div>
+    </div>
+  );
+}
+export default Home;
