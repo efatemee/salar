ae214dc02daea3e1ecdd89381605d9536864843c
fateme <117821877+efatemee@users.noreply.github.com>
2025-07-09 20:21:49 +0330

    title and date new article

--- src/pages/creatArticle/creatarticle.jsx
+++ src/pages/creatArticle/creatarticle.jsx
@@ -3,23 +3,40 @@ import Footer from "../../components/footer/footer";
 import styled from "./creatarticle.module.css";
 import { useState } from "react";
 function CreatArticle() {
-  const [counter, setcounter] = useState(0);
-  const increment = () => {
-    setcounter(counter + 1);
+  const [article, seArticle] = useState({
+    title: "",
+    date: "",
+  });
+  const changeart = (e) => {
+    switch (e.target.name) {
+      case "title":
+        seArticle({
+          title: e.target.value,
+        });
+        break;
+      case "date":
+        seArticle({
+          date: e.target.value,
+        });
+        break;
+    }
   };
+  console.log(article);
 
-  const decrement = () => {
-    setcounter(counter - 1);
-  };
   return (
     <div>
       <Navbar title="سالار" />
       <div className="container">
         <div className={styled.newarticle}>
           <h2>مقاله جدید</h2>
-          <button onClick={increment}>increment</button>
-          <span>{counter}</span>
-          <button onClick={decrement}>discrement</button>
+          <div className={styled.changeart}>
+            <label>عنوان</label>
+            <input name="title" onChange={changeart} type="text" />
+          </div>
+          <div className={styled.changeart}>
+            <label>تاریخ</label>
+            <input name="date" onChange={changeart} type="text" />
+          </div>
         </div>
       </div>
       <Footer />
